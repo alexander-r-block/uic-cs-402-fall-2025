c058900114f9699cbef6250eb43ef45d0740f8e7
Alexander R. Block <arblock@uic.edu>
2025-12-09 10:38:22 -0600

    Adding my code to upload.

---
+++ project-2/results/extra-credit/Alex_Block_project2.cpp
@@ -0,0 +1,14 @@
+Project 2 (Extra Credit) Test Results
+
+TestCase                                Result
+================================================================
+extra-credit.cpp                        2.0/4
+================================================================
+Result: 2/4
+
+Key:
+	Failed to Compile: Your submission did not compile due to a syntax or naming error
+	Compiled with warnings: Your submission uses unchecked or unsafe operations
+	Crashed due to signal SIGNAL_CODE: Your submission threw an uncaught exception.
+	All signal error codes are described here: https://man7.org/linux/man-pages/man7/signal.7.html
+	Exceeded Time Limit: Your submission took too much time to run (probably an infinite loop)
